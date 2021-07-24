c1a7da9c90ea399c3312c49de27fa5ee3d7f8f3e
maxmarkov <maxmarkov@gmail.com>
2021-07-24 23:28:06 +0200

    download script interfaced with YOLOv5

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
 # Export YOLOv5 to Google Coral Dev Mini
 
-## Step 0: Download PyTorch models 
+## Step 0: Preparation. Download models.
 
-Download PyTorch models from YOLOv5 repository:
+Clone the repository implementing inference of YOLOv5 models on Google Coral Dev Board Mini
 
 ```
-python download_models.py
+git clone https://github.com/maxmarkov/yolov5-coral-dev-mini.git
+```
+
+This repository uses YOLOv5 as submodule. Clone the submodule and checkout the latest stable release (v5.0 on July 24)
+
+```
+cd yolov5 && git submodule init && git submodule update && git checkout tags/v5.0
+```
+
+Install all necessary requirements 
+
+```
+pip install -r requirements && pip install onnx>=1.9.0
+```
+
+Download PyTorch models from YOLOv5 repository into models folder:
+
+```
+cd .. && python3 download_models.py --weights yolov5s.pt yolov5s6.pt 
 ```
 
 ## Step 1: Convert PyTorch model into TensorFlow Lite
--- download_models.py
+++ download_models.py
@@ -1,43 +1,28 @@
-from pathlib import Path
 import os
-import requests
+import shutil
+import argparse
+from yolov5.utils.google_utils import attempt_download
 
-def attempt_download(file, repo='ultralytics/yolov5'):
-    '''
-    Download models with curl. Torch downloader has been erased to reduce code dependences.
-    '''
-    # Attempt file download if does not exist
-    file = Path(str(file).strip().replace("'", '').lower())
-    
-    if not file.exists():
-        # github api
-        response = requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json()
-        assets = [x['name'] for x in response['assets']]                                        # release assets, i.e. ['yolov5s.pt', 'yolov5m.pt', ...]
-        tag = response['tag_name']                                                              # i.e. 'v1.0'
-        print(assets)
-
-        name = file.name
-        if name in assets:
-            try:
-                url = f'https://storage.googleapis.com/{repo}/ckpt/{name}'
-                print(f'Downloading {url} to {file}...')
-                os.system(f'curl -L {url} -o models/{file}')
-            except:
-                if not file.exists() or file.stat().st_size < 1E6:  # check
-                    file.unlink(missing_ok=True)  # remove partial downloads
-                    print(f'ERROR: Download failure: {msg}')
-                print('')
-                return
 
 if __name__ == '__main__':
+    '''
+    Download pre-trained models from YOLOv5 repository
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    opt = parser.parse_args()
 
-   ## --img 640 models
-   #files_640 = ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt']#, 'yolov5x.pt']
-   
-   ## --img 1280 models
-   #files_1280 = ['yolov5s6.pt', 'yolov5m6.pt', 'yolov5l6.pt', 'yolov5x6.pt']
-   
-   files = ['yolov5s.pt']
+    # --img 640 models
+    weights_640 = ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']
 
-   for file in files:
-      attempt_download(file)
+    # --img 1280 models
+    weights_1280 = ['yolov5s6.pt', 'yolov5m6.pt', 'yolov5l6.pt', 'yolov5x6.pt']
+
+    weights_repo = weights_640 + weights_1280
+
+    for weights in opt.weights:
+        if weights in weights_repo:
+            attempt_download(weights)
+            shutil.move(src=weights, dst=os.path.join('models', weights))
+        else:
+            print('File {} is not in YOLOv5 repository'.formatr(weights))
